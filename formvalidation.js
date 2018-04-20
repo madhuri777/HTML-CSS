2ac8d28854e9edc9542e3255c31ce39230d30bce
madhuri777 <satkarmadhuri777@gmail.com>
2018-04-20 19:25:49 +0530

    20

---
+++ formvalidation.js
@@ -0,0 +1,10 @@
+function formvalidation() {
+  var uid=document.registration.userid;
+  var pss=document.registration.pass;
+  var uname=document.registration.user_name;
+  var uadd=document.registration.add;
+  var ucountry=document.registration.country;
+  var uzip=document.registration.zipcode;
+  var uemail=document.registration.email;
+  //var =document.registration.;
+}
